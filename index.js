50247508f2419bfcbe9e47f939c177d06c8128d1
odogun maryam aderonke <odogunmaryam@gmail.com>
2023-07-25 12:00:58 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+let homeEl= document.getElementById("home-el");
+let count= 0;
+function button1(){
+count+=1
+homeEl.textContent= count;
+}
+function button2(){
+count+=2
+homeEl.textContent= count;
+}
+function button3(){
+count+=3
+homeEl.textContent= count;
+}
+
+
+let guestEl= document.getElementById("guest-el");
+let save= 0;
+function button4(){
+save+=1
+guestEl.textContent= save;
+}
+function button5(){
+save+=2
+guestEl.textContent= save;
+}
+function button6(){
+save+=3
+guestEl.textContent=save;
+}
